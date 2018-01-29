c06bbdf0465064145147251dd606ebe14b218a91
Jen Poole <contact.vandepool@gmail.com>
2018-01-29 00:21:05 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,10 @@
+class ChattyStudent
+
+  # returns a greeting phrase plus a long chatty phrase
+  def hello
+  end
+
+  # returns the phrase 'Pick me!' ten times.
+  def raise_hand
+  end
+end
---
+++ lib/student.rb
@@ -0,0 +1,10 @@
+class Student
+
+  # returns a greeting phrase
+  def hello
+  end
+
+  # returns the phrase 'Pick me!'
+  def raise_hand
+  end
+end
