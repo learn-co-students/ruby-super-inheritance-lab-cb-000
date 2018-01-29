f5130e59c59febfffe36f8e5e56dccc5b34a61a9
Jen Poole <contact.vandepool@gmail.com>
2018-01-29 00:30:03 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,10 +1,13 @@
-class ChattyStudent
+class ChattyStudent < Student
 
   # returns a greeting phrase plus a long chatty phrase
   def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
   # returns the phrase 'Pick me!' ten times.
   def raise_hand
+    10.times { super }
   end
 end
